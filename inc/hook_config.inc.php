c602ac7552512b9bd4eb4fd3cbe1a8ecbf4da06d
skwashd <skwashd@alumni.egroupware.org>
2003-03-17 03:21:45 +0000

    finally worked this out

--- inc/hook_config.inc.php
+++ inc/hook_config.inc.php
@@ -1,20 +1,23 @@
 <?php
-  /**************************************************************************\
-  * phpGroupWare                                                             *
-  * http://www.phpgroupware.org                                              *
-  * Written by Mark Peters <skeeter@phpgroupware.org>                        *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
+	/**************************************************************************\
+	* phpGroupWare - KnowledgeBase                                             *
+	* http://www.phpgroupware.org                                              *
+	* Written by Dave Hall [skwashd AT phpgroupware.org]		           *
+	* ------------------------------------------------------------------------ *
+	* Started off as a port of phpBrain - http://vrotvrot.com/phpBrain/	   *
+	*  but quickly became a full rewrite			                   *
+	* ------------------------------------------------------------------------ *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
 
   /* $Id$ */
 
 	function anon_user($config)
 	{
 		$sbox = createObject('phpgwapi.sbox2');
-		return $sbox->getAccount('newsettings[anon_user]',$config);//$longnames=0)
+		return $sbox->getAccount('newsettings[anon_user]',$config['anon_user'], true);
 	}
 ?>
