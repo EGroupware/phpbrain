1f77b391d3b9737fd22fdd401fa36118d63a440b
ceb <ceb@alumni.egroupware.org>
2003-04-23 01:51:27 +0000

    update hook_admin

--- inc/hook_admin.inc.php
+++ inc/hook_admin.inc.php
@@ -2,27 +2,26 @@
 	/**************************************************************************\
 	* phpGroupWare - KnowledgeBase                                             *
 	* http://www.phpgroupware.org                                              *
-	* Written by Dave Hall [dave.hall at mbox.com.au]			                 *
+	* Written by Dave Hall [dave.hall at mbox.com.au]			               *
 	* ------------------------------------------------------------------------ *
-	* Started off as a port of phpBrain - http://vrotvrot.com/phpBrain/		 *
-	*  but quickly became a full rewrite										 *
+	* Started off as a port of phpBrain - http://vrotvrot.com/phpBrain/		   *
+	* but quickly became a full rewrite                                        *
 	* ------------------------------------------------------------------------ *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
+	* This program is free software; you can redistribute it and/or modify it  *
+	* under the terms of the GNU General Public License as published by the    *
+	* Free Software Foundation; either version 2 of the License, or (at your   *
+	* option) any later version.                                               *
 	\**************************************************************************/
 
 	{
-		$values = array
+		$file = array
 		(
-			'Site Configuration' => $GLOBALS['phpgw']->link('/index.php','menuaction=admin.uiconfig.index&appname=' . $appname),
-			'Global Categories' => $GLOBALS['phpgw']->link('/index.php','menuaction=admin.uicategories.index&appname=' . $appname),
-			'Maintain Answers' => $GLOBALS['phpgw']->link('/index.php','menuaction=phpbrain.uikb.maint_answer'),
-			'Maintain Questions' => $GLOBALS['phpgw']->link('/index.php','menuaction=phpbrain.uikb.maint_question'),
-			'Reports - Not implemented' => $GLOBALS['phpgw']->link() //$GLOBALS['phpgw']->link('/index.php','menuaction=phpbrain.uikb.index')
+			'Site Configuration'		=> $GLOBALS['phpgw']->link('/index.php','menuaction=admin.uiconfig.index&appname=' . $appname),
+			'Global Categories'			=> $GLOBALS['phpgw']->link('/index.php','menuaction=admin.uicategories.index&appname=' . $appname),
+			'Maintain Answers'			=> $GLOBALS['phpgw']->link('/index.php','menuaction=phpbrain.uikb.maint_answer'),
+			'Maintain Questions'		=> $GLOBALS['phpgw']->link('/index.php','menuaction=phpbrain.uikb.maint_question'),
+			'Reports - Not implemented'	=> $GLOBALS['phpgw']->link() //$GLOBALS['phpgw']->link('/index.php','menuaction=phpbrain.uikb.index')
 		);
-
-		display_section($appname,$appname,$values);
+		$GLOBALS['phpgw']->common->display_mainscreen($appname,$file);
 	}
 ?>
