12410247ff544cec77f28426259a6bf50b657b99
Miles Lott <milosch@alumni.egroupware.org>
2004-01-26 05:47:29 +0000

    formatting

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 	/**************************************************************************\
 	* phpGroupWare - KnowledgeBase                                             *
 	* http://www.phpgroupware.org                                              *
-	* Written by Dave Hall [skwashd AT phpgroupware.org]		                 *
+	* Written by Dave Hall [skwashd AT phpgroupware.org]                       *
 	* ------------------------------------------------------------------------ *
-	* Started off as a port of phpBrain - http://vrotvrot.com/phpBrain/		 *
-	*  but quickly became a full rewrite										 *
+	* Started off as a port of phpBrain - http://vrotvrot.com/phpBrain/        *
+	*  but quickly became a full rewrite.                                      *
 	* ------------------------------------------------------------------------ *
 	*  This program is free software; you can redistribute it and/or modify it *
 	*  under the terms of the GNU General Public License as published by the   *
@@ -14,15 +14,16 @@
 	\**************************************************************************/
 	/* $Id$ */
 
-	$GLOBALS['phpgw_info']['flags'] = array
-	(
+	$phpgw_info = array();
+	$GLOBALS['phpgw_info']['flags'] = array(
 		'currentapp' => 'phpbrain',
 		'noheader'   => True,
 		'nonavbar'   => True
 	);
 	include('../header.inc.php');
 
-	header('Location: ' . $GLOBALS['phpgw']->link('/index.php', 
-											array('menuaction' => 'phpbrain.uikb.index')));
+	$obj = CreateObject('phpbrain.uikb');
+	$obj->index();
+
 	$GLOBALS['phpgw']->common->phpgw_exit();
 ?>
--- setup/setup.inc.php
+++ setup/setup.inc.php
@@ -1,6 +1,6 @@
 <?php
 	/**************************************************************************\
-	* eGroupWare - Addressbook                                                 *
+	* eGroupWare - PHPBrain                                                    *
 	* http://www.egroupware.org                                                *
 	* --------------------------------------------                             *
 	*  This program is free software; you can redistribute it and/or modify it *
@@ -21,8 +21,7 @@
 	$setup_info['phpbrain']['author'] = 'Dave Hall';
 	$setup_info['phpbrain']['note']   = 'A knowledge base for storing and searching for FAQs and Tutorials';
 	$setup_info['phpbrain']['license']  = 'GPL';
-	$setup_info['phpbrain']['description'] =
-		'Searchable Knowledge Base.';
+	$setup_info['phpbrain']['description'] = 'Searchable Knowledge Base.';
 	$setup_info['phpbrain']['maintainer'] = 'Alejandro Pedraza';
 	$setup_info['phpbrain']['maintainer_email'] = 'alpeb@users.sourceforge.net';
 
@@ -40,14 +39,14 @@
 	//$setup_info['phpbrain']['hooks'][] = 'notifywindow';
 	//$setup_info['phpbrain']['hooks'][] = 'preferences';
 	$setup_info['phpbrain']['hooks'][] = 'sidebox_menu';
-	
+
 	$setup_info['phpbrain']['tables'][] = 'phpgw_kb_faq';
 	$setup_info['phpbrain']['tables'][] = 'phpgw_kb_comment';
 	$setup_info['phpbrain']['tables'][] = 'phpgw_kb_questions';
 
 	/* Dependencies for this app to work */
 	$setup_info['phpbrain']['depends'][] = array(
-		 'appname' => 'phpgwapi',
-		 'versions' => Array('0.9.13', '0.9.14', '0.9.15')
+		'appname' => 'phpgwapi',
+		'versions' => Array('0.9.13', '0.9.14', '0.9.15')
 	);
 ?>
