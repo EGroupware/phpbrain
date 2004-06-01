dfd78465619fd5257c07c7146ccc24068126f96a
alpeb <alpeb@alumni.egroupware.org>
2004-06-01 05:06:20 +0000

    Fix for postgresql

--- inc/class.sokb.inc.php
+++ inc/class.sokb.inc.php
@@ -41,7 +41,7 @@
 		{
 			$order = $this->db->db_addslashes($order);
 			if ($sort != 'DESC') $sort = 'ASC';
-			$fields = array('DISTINCT phpgw_kb_articles.art_id', 'title', 'topic', 'views', 'cat_id', 'published', 'user_id', 'created', 'modified', 'votes_1', 'votes_2', 'votes_3', 'votes_4', 'votes_5', 'files');
+			$fields = array('DISTINCT phpgw_kb_articles.art_id', 'title', 'topic', 'views', 'cat_id', 'published', 'user_id', 'created', 'modified', 'votes_1', 'votes_2', 'votes_3', 'votes_4', 'votes_5', 'files', 'score');
 			$fields_str = implode(', ', $fields);
 			$owners = implode(', ', $owners);
 
