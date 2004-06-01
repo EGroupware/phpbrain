3de86862f54a956571d6054a52ef1b7d2025c0e9
alpeb <alpeb@alumni.egroupware.org>
2004-06-01 05:11:41 +0000

    various changed to make it work with postgres

--- setup/tables_update.inc.php
+++ setup/tables_update.inc.php
@@ -2,6 +2,8 @@
 	$test[] = '0.9.14.001';
 	function phpbrain_upgrade0_9_14_001()
 	{
+		global $DEBUG;
+
 		$db1 = $GLOBALS['phpgw_setup']->db;
 
 		$GLOBALS['phpgw_setup']->oProc->CreateTable(
@@ -43,6 +45,7 @@
 				'uc' => array()
 			)
 		);
+		if ($DEBUG) echo '<br>tables_update: new tables created';
 
 		$GLOBALS['phpgw_setup']->oProc->RenameTable('phpgw_kb_faq', 'phpgw_kb_articles');
 		$GLOBALS['phpgw_setup']->oProc->RenameColumn('phpgw_kb_articles', 'faq_id', 'art_id');
@@ -58,9 +61,11 @@
 		$GLOBALS['phpgw_setup']->oProc->AddColumn('phpgw_kb_articles', 'votes_4', array('type' => 'int', 'precision' => '4', 'nullable' => False));
 		$GLOBALS['phpgw_setup']->oProc->AddColumn('phpgw_kb_articles', 'votes_5', array('type' => 'int', 'precision' => '4', 'nullable' => False));
 		$GLOBALS['phpgw_setup']->oProc->AddColumn('phpgw_kb_articles', 'files', array('type' => 'text', 'nullable' => False));
+		if ($DEBUG) echo '<br>tables_update: added columns to phpgw_kb_articles';
 
 		$sql = "SELECT art_id, user_id, modified, urls, votes, total FROM phpgw_kb_articles";
 		$GLOBALS['phpgw_setup']->oProc->query($sql, __LINE__, __FILE__);
+		if ($DEBUG) echo '<br>tables_update: query on phpgw_kb_articles executed';
 		while($GLOBALS['phpgw_setup']->oProc->next_record())
 		{
 			$art_id		= $GLOBALS['phpgw_setup']->oProc->f('art_id');
@@ -81,13 +86,26 @@
 			}
 			if (!$votes)
 			{
-				$sql = "UPDATE phpgw_kb_articles SET created=$modified, modified_user_id=$user_id, urls='$new_urls' WHERE art_id=$art_id";
+				$average = -1;
 			}
 			else
 			{
 				$average = round($total / $votes);
-				$sql = "UPDATE phpgw_kb_articles SET created=$modified, modified_user_id=$user_id, votes_". $average ."=$votes, urls='$new_urls' WHERE art_id=$art_id";
 			}
+			$votes_str = array();
+			for ($j=1; $j<=5; $j++)
+			{
+				if ($j == $average)
+				{
+					$votes_str[] = "votes_" . $j . "=$votes";
+				}
+				else
+				{
+					$votes_str[] = "votes_" . $j . "=0";
+				}
+			}
+			$votes_str = implode(', ', $votes_str);
+			$sql = "UPDATE phpgw_kb_articles SET created=$modified, modified_user_id=$user_id, $votes_str, urls='$new_urls', q_id='0', topic='', files=''  WHERE art_id=$art_id";
 			$db1->query($sql, __LINE__, __FILE__);
 
 			$sql = "INSERT INTO phpgw_kb_search (keyword, art_id, score) VALUES ('', $art_id, 1)";
@@ -96,27 +114,27 @@
 		$new_table_def = array(
 			'fd' => array(
 				'art_id'			=> array('type' => 'auto','nullable' => False),
-				'q_id'				=> array('type' => 'int', 'precision' => 8, 'nullable' => False),
 				'title'				=> array('type' => 'text','nullable' => False),
-				'topic'				=> array('type' => 'text', 'nullable' => False),
 				'text'				=> array('type' => 'text','nullable' => False),
 				'cat_id'			=> array('type' => 'int','precision' => '4','nullable' => False,'default' => '0'),
 				'published' 		=> array('type' => 'int','precision' => '2','nullable' => False,'default' => '0'),
 				'keywords'			=> array('type' => 'text','nullable' => False),
 				'user_id'			=> array('type' => 'int','precision' => '4','nullable' => False,'default' => '0'),
 				'views'				=> array('type' => 'int','precision' => '4','nullable' => False,'default' => '0'),
-				'created'			=> array('type' => 'int','precision' => '4','nullable' => True),
 				'modified'			=> array('type' => 'int','precision' => '4','nullable' => True),
-				'modified_user_id'	=> array('type' => 'int','precision' => '4','nullable' => False),
-				'files'				=> array('type' => 'text', 'nullable' => False),
 				'urls'				=> array('type' => 'text', 'nullable' => False),
 				'votes'				=> array('type' => 'int','precision' => '4','nullable' => False,'default' => '0'),
 				'total'				=> array('type' => 'int','precision' => '4','nullable' => False,'default' => '0'),
+				'q_id'				=> array('type' => 'int', 'precision' => 8, 'nullable' => False),
+				'topic'				=> array('type' => 'text', 'nullable' => False),
+				'created'			=> array('type' => 'int','precision' => '4','nullable' => True),
+				'modified_user_id'	=> array('type' => 'int','precision' => '4','nullable' => False),
 				'votes_1'			=> array('type' => 'int','precision' => '4','nullable' => False),
 				'votes_2'			=> array('type' => 'int','precision' => '4','nullable' => False),
 				'votes_3'			=> array('type' => 'int','precision' => '4','nullable' => False),
 				'votes_4'			=> array('type' => 'int','precision' => '4','nullable' => False),
-				'votes_5'			=> array('type' => 'int','precision' => '4','nullable' => False)
+				'votes_5'			=> array('type' => 'int','precision' => '4','nullable' => False),
+				'files'				=> array('type' => 'text', 'nullable' => False)
 			),
 			'pk' => array('art_id'),
 			'fk' => array(),
@@ -124,34 +142,45 @@
 			'uc' => array()
 		);
 		$GLOBALS['phpgw_setup']->oProc->DropColumn('phpgw_kb_articles', $new_table_def, 'is_faq');
-		unset($newdef['fd']['votes']);
+		if ($DEBUG) echo '<br>tables_update: dropped column is_faq in phpgw_kb_articles';
+		unset($new_table_def['fd']['votes']);
 		$GLOBALS['phpgw_setup']->oProc->DropColumn('phpgw_kb_articles', $new_table_def, 'votes');
-		unset($newdef['fd']['total']);
+		if ($DEBUG) echo '<br>tables_update: dropped column votes in phpgw_kb_articles';
+		unset($new_table_def['fd']['total']);
 		$GLOBALS['phpgw_setup']->oProc->DropColumn('phpgw_kb_articles', $new_table_def, 'total');
+		if ($DEBUG) echo '<br>tables_update: dropped column total in phpgw_kb_articles';
 
 		$GLOBALS['phpgw_setup']->oProc->RenameColumn('phpgw_kb_comment', 'faq_id', 'art_id');
+		if ($DEBUG) echo '<br>tables_update: renamed column faq_id to art_id in phpgw_kb_articles';
 		$GLOBALS['phpgw_setup']->oProc->AddColumn('phpgw_kb_comment', 'published', array('type' => 'int', 'precision' => '2', 'nullable' => False));
+		if ($DEBUG) echo '<br>tables_update: added column published in phpgw_kb_articles';
 		$sql = "UPDATE phpgw_kb_comment SET published=1";
 		$db1->query($sql, __LINE__, __FILE__);
 
 
 		$GLOBALS['phpgw_setup']->oProc->AddColumn('phpgw_kb_questions', 'user_id', array('type' => 'int', 'precision' => '4', 'nullable' => False));
-		$GLOBALS['phpgw_setup']->oProc->RenameColumn('phpgw_kb_questions', 'question', 'summary');
+		if ($DEBUG) echo '<br>tables_update: added column user_id in phpgw_kb_questions';
 		$GLOBALS['phpgw_setup']->oProc->AddColumn('phpgw_kb_questions', 'details', array('type' => 'text', 'nullable' => False));
+		if ($DEBUG) echo '<br>tables_update: added column details in phpgw_kb_questions';
 		$GLOBALS['phpgw_setup']->oProc->AddColumn('phpgw_kb_questions', 'cat_id', array('type' => 'int', 'precision' => '4', 'nullable' => False, 'default' => '0'));
+		if ($DEBUG) echo '<br>tables_update: added column cat_id in phpgw_kb_questions';
 		$GLOBALS['phpgw_setup']->oProc->AddColumn('phpgw_kb_questions', 'creation', array('type' => 'int', 'precision' => '4', 'nullable' => True));
-		$GLOBALS['phpgw_setup']->oProc->RenameColumn('phpgw_kb_questions', 'pending', 'published');
+		if ($DEBUG) echo '<br>tables_update: added column creation in phpgw_kb_questions';
 
-		$sql = "SELECT question_id, published FROM phpgw_kb_questions";
+		$sql = "SELECT question_id, pending FROM phpgw_kb_questions";
 		$GLOBALS['phpgw_setup']->oProc->query($sql, __LINE__, __FILE__);
 		while($GLOBALS['phpgw_setup']->oProc->next_record())
 		{
 			$question_id	= $GLOBALS['phpgw_setup']->oProc->f('question_id');
-			$published		= $GLOBALS['phpgw_setup']->oProc->f('published');
+			$published		= $GLOBALS['phpgw_setup']->oProc->f('pending');
 			$published = $published? 0 : 1;
-			$sql = "UPDATE phpgw_kb_questions SET published=$published WHERE question_id=$question_id";
+			$sql = "UPDATE phpgw_kb_questions SET pending=$published, user_id='0', details='', cat_id='0', creation='". time() ."' WHERE question_id=$question_id";
 			$db1->query($sql, __LINE__, __FILE__);
 		}
+		$GLOBALS['phpgw_setup']->oProc->RenameColumn('phpgw_kb_questions', 'question', 'summary');
+		if ($DEBUG) echo '<br>tables_update: renamed column question to summary in phpgw_kb_questions';
+		$GLOBALS['phpgw_setup']->oProc->RenameColumn('phpgw_kb_questions', 'pending', 'published');
+		if ($DEBUG) echo '<br>tables_update: renamed column pending to published in phpgw_kb_questions';
 
 		$GLOBALS['setup_info']['phpbrain']['currentver'] = '1.0RC5';
 		return $GLOBALS['setup_info']['phpbrain']['currentver'];
