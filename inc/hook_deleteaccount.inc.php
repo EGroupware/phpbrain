cb7ee70dfa62a2f31cac925b18748901ac67e087
Lars Kneschke <lkeschke@alumni.egroupware.org>
2005-09-20 10:11:38 +0000

    modified the delete hooks to use $GLOBALS['hook_values']

--- inc/hook_deleteaccount.inc.php
+++ inc/hook_deleteaccount.inc.php
@@ -1,14 +1,17 @@
 <?php
 	/* $Id$ */
 
-	$bokb = CreateObject('phpbrain.bokb');
+	if((int)$GLOBALS['hook_values']['account_id'] > 0)
+	{
+		$bokb = CreateObject('phpbrain.bokb');
 
-	if((int)$_POST['new_owner'] == 0)
-	{
-		$bokb->delete_owner_articles((int)$_POST['account_id']);
-	}
-	else
-	{
-		$bokb->change_articles_owner((int)$_POST['account_id'],(int)$_POST['new_owner']);
+		if((int)$_POST['new_owner'] == 0)
+		{
+			$bokb->delete_owner_articles((int)$GLOBALS['hook_values']['account_id']);
+		}
+		else
+		{
+			$bokb->change_articles_owner((int)$GLOBALS['hook_values']['account_id'],(int)$_POST['new_owner']);
+		}
 	}
 ?>
